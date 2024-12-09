be291f981a7686b042d18f5a448b1d1fea681836
Thomas Ball <thomasjball@users.noreply.github.com>
2024-12-09 18:11:08 +0000

    remove server side

--- gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const wrapPageElement = ({ element, props }) => {
-    return <div {...props}>{element}</div>
-}
-
-export const onRenderBody = ({ setHtmlAttributes }) => {
-    setHtmlAttributes({ lang: "en" })
-}
